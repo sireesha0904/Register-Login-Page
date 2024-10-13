181509920a0b27ef462303a78771216fee1d5b13
ModiSireesha <modisireesha09@gmail.com>
2024-10-13 20:13:17 +0530

    updated

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,5 +7,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  signUpForm: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.checkPasswords }
+    );
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPassword() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  checkPasswords(group: FormGroup) {
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value;
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+      // Perform sign-up action here
+    }
+  }
 }
